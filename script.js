3e4b3ecf9e8070b1179179063dbd3104ae2eca2e
Germann Kuznetsov <germann.kuznetsov@gmail.com>
2018-03-19 17:25:57 +0300

    add canvas

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-var synth = new Tone.AMSynth().toMaster()
+(function(){
+  var canvasWidth = 16;
+  var canvasHeight= 8;
+  var pieceWidth = 60;
+  var pieceHeight= 40;
+  var width = 1 + (canvasWidth * pieceWidth);
+  var height= 1 + (canvasHeight * pieceHeight);
 
-//attach a listener to all of the buttons
-document.querySelectorAll('button').forEach(function(button){
-	button.addEventListener('mousedown', function(e){
-		//play the note on mouse down
-		synth.triggerAttack(e.target.textContent)
-	})
-	button.addEventListener('mouseup', function(e){
-		//release on mouseup
-		synth.triggerRelease()
-	})
-})
+  var canvas;
+
+  function drawBoard() {
+    var context = canvas.getContext("2d");
+    context.clearRect(0, 0, width, height);
+
+      context.beginPath();
+
+      /* vertical lines */
+      for (var x = 0; x <= width; x += pieceWidth) {
+  	context.moveTo(0.5 + x, 0);
+  	context.lineTo(0.5 + x, height);
+      }
+
+      /* horizontal lines */
+      for (var y = 0; y <= height; y += pieceHeight) {
+  	context.moveTo(0, 0.5 + y);
+  	context.lineTo(width, 0.5 +  y);
+      }
+
+      /* draw it! */
+      context.strokeStyle = "#ccc";
+      context.stroke();
+  }
+  function init () {
+    canvas = document.getElementById("note_canvas");
+    canvas.width = width;
+    canvas.height = height;
+    drawBoard();
+    var a = 1;
+  }
+
+  init();
+})();
